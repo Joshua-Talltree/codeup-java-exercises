c3683483e2afc4354289cef50cf50e26f910979a
Joshua-Talltree <joshua.scott.talltree@gmail.com>
2021-01-22 14:52:02 -0600

    ConsoleExercises pt 4.

--- src/syntax/ConsoleExercises.java
+++ src/syntax/ConsoleExercises.java
@@ -14,17 +14,17 @@ public class ConsoleExercises {
 // TODO: Write some Java code that uses the variable pi to output the following: The value of pi is approximately 3.14.
 
         PrintStream format;
-        System.out.printf("The value of pi is approximately: %s\n", pi);
+        System.out.printf("The value of pi is approximately: %.2f\n", pi);
 
 // TODO: Don't change the value of the variable; instead, reference one of the links above and use System.out.format to accomplish this.
 
-        System.out.format("The value of pi is now: 3.0\n", pi);
+        System.out.format("The value of pi is now: %.3f\n", pi);
 
 // TODO: Prompt a user to enter a integer and store that value in an int variable using the nextInt method.
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter something: ");
+        System.out.print("Enter an int: ");
         int number = scanner.nextInt();
 
         System.out.println("You entered: --> \"" + number + "\" <--");
@@ -50,6 +50,7 @@ public class ConsoleExercises {
 // TODO: Prompt a user to enter a sentence, then store that sentence in a String variable using the next method. Then, display that sentence back to the user. do you capture all the words?
 
         System.out.print("Please enter a sentence here: ");
+//        String oneSentence = scanner.next(); // only grabs the fisrt token and then sees the white space and stops
         String oneSentence = scanner.nextLine();
 
         System.out.println("Your sentence: \"" + oneSentence + "\"");
@@ -73,10 +74,12 @@ public class ConsoleExercises {
 // TODO: Display the area and perimeter of that classroom. The area of a rectangle is equal to the length times the width, and the perimeter of a rectangle is equal to 2 times the length plus 2 times the width.
 
         System.out.print("Enter the length, the width, and area measurement here:");
-        String lengthWidthEquals = scanner.nextLine();
+        System.out.print("Enter the length and width, multiplied by 2 here:");
+//        String lengthWidthEquals = scanner.nextLine();
+        String lengthTwoWidthTwoEquals = scanner.nextLine();
 
-        System.out.println("The area of the room is: " + lengthWidthEquals);
-        System.out.println("The perimeter of a room is: " + lengthWidthEquals);
+//        System.out.println("The area of the room is: " + lengthWidthEquals);
+        System.out.println("The perimeter of a room is: " + lengthTwoWidthTwoEquals);
 
 
     }
