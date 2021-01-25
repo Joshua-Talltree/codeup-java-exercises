6fa48064c55da29a2ea2cfa346b196f0c6a2084f
Joshua-Talltree <joshua.scott.talltree@gmail.com>
2021-01-25 11:58:47 -0600

    Sting and Bob exercises

---
+++ src/StringExercise.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class StringExercise {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // TODO: String Basics. Create a class named StringExercise with a main method. For each of the following output examples: create a String variable that contains the desired output and print it out to the console, you can do this with only one String variable and one print statement for each output example.
+
+        String retResult = "";
+
+        System.out.println("Enter phrase here: " + retResult);
+        retResult = sc.nextLine();
+
+        System.out.println(retResult);
+
+    }
+}
+
+
