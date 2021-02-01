e6386876c66951ecc1307596c7c19ddd18b4eba1
Joshua-Talltree <joshua.scott.talltree@gmail.com>
2021-02-01 13:50:16 -0600

    Circles commit

---
+++ src/shapes/Circle.java
@@ -0,0 +1,17 @@
+package shapes;
+
+public class Circle {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getArea() {
+
+    }
+}
--- src/util/Input.java
+++ src/util/Input.java
@@ -2,7 +2,7 @@ package util;
 
 import java.util.Scanner;
 
-public class Input() {
+public class Input {
 
     private Scanner scanner;
 
@@ -11,22 +11,22 @@ public class Input() {
         this.scanner = new Scanner(System.in);
     }
 
-    String getString() {
+    public String getString() {
         return this.scanner.nextLine();
     }
 
-    boolean yesNo(){
+    public boolean yesNo(){
         return this.scanner.nextBoolean();
         String answer = this.scanner.nextLine();
         return (answer.toLowerCase().equals("Y") ||
                 answer.toLowerCase().equals("yes"));
     }
 
-    int getInt(){
+    public int getInt(){
         return this.scanner.nextInt();
     }
 
-    int getInt(int min int max) {
+    public int getInt(int min int max) {
         while (true) {
             int answer = this.scanner.nextInt();
             if (answer >= min && answer <= max)
@@ -34,11 +34,11 @@ public class Input() {
         }
     }
 
-    double getDouble(){
+    public double getDouble(){
         return this.scanner.nextDouble();
     }
 
-    double getDouble(double min double max) {
+    public double getDouble(double min double max) {
         while (true) {
             double answer = this.scanner.nextDouble();
             if (answer >= min && answer <= max)
