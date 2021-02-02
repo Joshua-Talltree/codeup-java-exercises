af4ed267714a5d4ce51006d6223acc26a840336e
Joshua-Talltree <joshua.scott.talltree@gmail.com>
2021-02-02 11:29:03 -0600

    Next and first Server name gen and Movie files

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -2,10 +2,18 @@ import java.util.Random;
 
 public class ServerNameGenerator {
 
-    public String[] adj = {"good", "bad", "overbearing", "pretentious", "insidious", "lethargic", "defiant", "cruel", "dull", "fragile" };
+    static String[] adj = {"good", "bad", "overbearing", "pretentious", "insidious", "lethargic", "defiant", "cruel", "dull", "fragile" };
 
-    public String[] nouns = {"house", "cat", "girl", "Spain", "foot", "Sony", "mouse", "tooth", "table", "apple"};
+    static String[] nouns = {"house", "cat", "girl", "Spain", "foot", "Sony", "mouse", "tooth", "table", "apple"};
 
-    Random rand = new Random();
+    static Random rand1 = new Random();
+    static Random rand2 = new Random();
+
+    static int randAd = rand1.nextInt(10);
+    static int randNoun = rand2.nextInt(10);
+
+    public static void main(String[] args) {
+        System.out.println("My server's new name is: " + adj[randAd] + " " + nouns[randNoun]);
+    }
 
 }
--- src/movies/Movie.java
+++ src/movies/Movie.java
@@ -3,5 +3,6 @@ package movies;
 public class Movie {
 
     public Movie(String citizen_kane, String drama) {
+
     }
 }
