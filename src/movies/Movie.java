8f3bf533887f15bcb4b8a8e9e1e064543dae0f48
Joshua-Talltree <joshua.scott.talltree@gmail.com>
2021-02-03 10:11:32 -0600

    Second commit with the movies app appended..

--- src/movies/Movie.java
+++ src/movies/Movie.java
@@ -2,7 +2,27 @@ package movies;
 
 public class Movie {
 
-    public Movie(String citizen_kane, String drama) {
+    private String name;
+    private String category;
 
+    public Movie(String name, String category) {
+        this.name = name;
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
     }
 }
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -3,6 +3,6 @@ package shapes;
 public class Square extends Rectangle {
 
     public Square(int side) {
-        super(side, side);
+        super(4 * side, side ^ 2);
     }
 }
