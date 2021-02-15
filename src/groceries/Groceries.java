b574e1e5619e82ac0b2cad03bb5dd1dc58397d53
Joshua-Talltree <joshua.scott.talltree@gmail.com>
2021-02-14 20:52:14 -0600

    GroceryList App pt. 2

--- src/groceries/Groceries.java
+++ src/groceries/Groceries.java
@@ -1,77 +1,88 @@
-//package groceries;
-//
-//
-//import java.util.ArrayList;
-//
-//public class Groceries {
-//
-//
-//    public ArrayList getGroceries() {
-//        return this.groceries;
-//    }
-//
-//    // variables
-//    private String vegetables;
-//    private String dairy;
-//    private String meat;
-//    private String fruits;
-//    private ArrayList<String> groceries;
-//
-//    // groceries constructor
-//    public Groceries(String vegetables, String dairy, String meat, String fruits) {
-//        this.vegetables = vegetables;
-//        this.dairy = dairy;
-//        this.meat = meat;
-//        this.fruits = fruits;
-//    }
-//
-//    public String getVegetables() {
-//        return this.vegetables;
-//    }
-//
-//    public String getDairy() {
-//        return dairy;
-//    }
-//
-//    public String getMeat() {
-//        return meat;
-//    }
-//
-//    public String getFruits() {
-//        return fruits;
-//    }
-//
-//    public void setVegetables(String vegetables) {
-//        this.vegetables = vegetables;
-//    }
-//
-//    public void setDairy(String dairy) {
-//        this.dairy = dairy;
-//    }
-//
-//    public void setMeat(String meat) {
-//        this.meat = meat;
-//    }
-//
-//    public void setFruits(String fruits) {
-//        this.fruits = fruits;
-//    }
-//
-//    public void setGroceries(ArrayList<String> groceries) {
-//        this.groceries = groceries;
-//    }
-//
-//    public void addVegetables(String vegetables) {
-//
-//    }
-//
-//    public static void main(String[] args) {
-//
-//        Groceries newVegetable = new Groceries("Tomatoes");
-//        newVegetable.addVegetables("Cherry Tomatoes");
-//        newVegetable.addVegetables("Roma Tomatoes");
-//        newVegetable.addVegetables("Beef Steak Tomatoes");
-//        newVegetable.addVegetables("Heirloom Tomatoes");
-//        System.out.println(newVegetable.getVegetables());
-//    }
-//}
+package groceries;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Groceries {
+
+
+    public ArrayList getGroceries() {
+        return this.groceries;
+    }
+
+    // variables
+    private String vegetables;
+    private String dairy;
+    private String meat;
+    private String fruits;
+    private ArrayList<String> groceries;
+
+    // groceries constructor
+    public Groceries(String vegetables, String dairy, String meat, String fruits) {
+        this.vegetables = vegetables;
+        this.dairy = dairy;
+        this.meat = meat;
+        this.fruits = fruits;
+    }
+
+    public String getVegetables() {
+        return vegetables;
+    }
+
+    public String getDairy() {
+        return dairy;
+    }
+
+    public String getMeat() {
+        return meat;
+    }
+
+    public String getFruits() {
+        return fruits;
+    }
+
+    public void setVegetables(String vegetables) {
+        this.vegetables = vegetables;
+    }
+
+    public void setDairy(String dairy) {
+        this.dairy = dairy;
+    }
+
+    public void setMeat(String meat) {
+        this.meat = meat;
+    }
+
+    public void setFruits(String fruits) {
+        this.fruits = fruits;
+    }
+
+    public void setGroceries(ArrayList<String> groceries) {
+        this.groceries = groceries;
+    }
+
+    public void addVegetables(String vegetables) {
+
+    }
+
+    public static void main(String[] args) {
+        Scanner groceryScanner = new Scanner(System.in);
+        Groceries newGroceries = new Groceries("Tomatoes", "Milk", "Ribeye", "Apples");
+
+        System.out.println("Which item would you like to add to your list: ");
+        String groceryAdder = groceryScanner.nextLine();
+
+
+        HashMap<String, String> groceryList = new HashMap<>();
+        groceryList.put("Milk ", " dairy");
+        groceryList.put("Carrots ", " vegetables");
+        groceryList.put("Ground beef ", " meat");
+        groceryList.put("Cheese ", " dairy");
+        groceryList.put("Tomatoes ", " vegetables");
+        groceryList.put("Pork chops ", " meat");
+
+        System.out.println(groceryList);
+    }
+
+}
