68d9055d1948045ad082ce170cd09833e62a1bf3
Joshua-Talltree <joshua.scott.talltree@gmail.com>
2021-03-04 09:19:28 -0600

    HighLow guessing game..

--- src/HighLow.java
+++ src/HighLow.java
@@ -1,2 +1,28 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class HighLow {
+
+
+    public static void main(String[] args) {
+
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+        int number = random.nextInt(100);
+        int guess = -1;
+
+
+        while (guess != number) {
+            System.out.print("Enter your guess: ");
+            guess = scanner.nextInt();
+            if (guess < number) {
+                System.out.println("Too low, please try again");
+            } else if (guess > number) {
+                System.out.println("Too high, please try again");
+            } else {
+                System.out.println("Correct, the number was " + number);
+            }
+        }
+    }
 }
+
